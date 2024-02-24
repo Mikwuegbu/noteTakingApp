ff271e2babe20e38f9e9b82b19b20110c1847f3a
Mikwuegbu <mstephen130@yahoo.com>
2024-02-24 23:14:31 +0100

    progressing

--- dist/scripts/script1.js
+++ dist/scripts/script1.js
@@ -3,7 +3,6 @@ const textArea = document.getElementById('post');
 const submitBtn = document.getElementById('create-btn');
 const formData = [];
 const cancelBtn = document.getElementById('cancel-btn');
-const editBtn = document.getElementById('edit-btn');
 
 //creating the id
 const uniqueId = () => {
@@ -24,21 +23,34 @@ const newPost = {
 	note: textArea.value,
 };
 
+//Clear the inputs
+const clearInputs = () => {
+	titlePost.value = '';
+	textArea.value = '';
+};
+
+//Validating form Inputs
+const formValidate = () => {
+	const validate = document.getElementById('validate');
+	if (!titlePost.value && !textArea.value) {
+		validate.innerHTML = '*Input fields cannot be empty';
+	} else if (!titlePost.value || !textArea.value) {
+		validate.innerHTML = '*One input field cannot be empty';
+	} else {
+		validate.innerHTML = '';
+		clearInputs();
+	}
+};
 //submit button fun
 const submitFn = (event) => {
-	// validating inputs
-	if (!titlePost.value && !textArea.value) {
-		event.preventDefault();
-		document.getElementById('validate').innerHTML =
-			'*Input fields cannot be empty';
-	} else if (!titlePost.value || !textArea.value) {
-		event.preventDefault();
-		document.getElementById('validate').innerHTML =
-			'*One input field cannot be empty';
-	}
+	event.preventDefault();
+	formValidate();
 
+	//storing to the localStorage
 	formData.push(newPost);
 	localStorage.setItem('formData', JSON.stringify(formData));
+
+	//document.getElementById('validate').append('Hellow word');
 };
 
 //Cancel button
