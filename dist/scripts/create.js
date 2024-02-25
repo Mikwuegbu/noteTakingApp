20ac1b32c412df2ae0e8e944f11f5b4d49eb7f78
Mikwuegbu <mstephen130@yahoo.com>
2024-02-25 08:03:28 +0100

    lots of bugs

--- dist/scripts/create.js
+++ dist/scripts/create.js
@@ -1,27 +1,41 @@
 const titlePost = document.getElementById('title');
 const textArea = document.getElementById('post');
 const submitBtn = document.getElementById('create-btn');
-const formData = [];
+// const formData = [];
 const cancelBtn = document.getElementById('cancel-btn');
 
 //creating the id
-const uniqueId = () => {
-	//checking for existing id
-	const initialId = localStorage.getItem('initialId');
-	//if found increment else create new one
-	const currentId = initialId ? parseInt('initialId') + 1 : 1;
+// const uniqueId = () => {
+// 	//checking for existing id
+// 	const initialId = localStorage.getItem('initialId');
+// 	//if found increment else create new one
+// 	const currentId = initialId ? parseInt('initialId') + 1 : 1;
 
-	//update the initialId
-	localStorage.setItem('initialId', currentId);
-	return currentId;
-};
+// 	//update the initialId
+// 	localStorage.setItem('initialId', currentId);
+// 	return currentId;
+// };
+// const createNote = () => {
+// 	const note = localStorage.getItem('note');
+// 	if (!note) {
+// 		localStorage.setItem('note', JSON.stringify([]));
+// 	}
+// };
+
+// const Notes = localStorage.getItem('note');
+// const formdata = JSON.parse(note);
+// const title = titlePost.value;
+// const note = textArea.value;
+// const id = formdata.length + 1;
+
+// const formData = [...formdata];
 
 //new Object
-const newPost = {
-	id: uniqueId(),
-	title: titlePost.value,
-	note: textArea.value,
-};
+// const newPost = {
+// 	id,
+// 	title: titlePost.value,
+// 	note: textArea.value,
+// };
 
 //Clear the inputs
 const clearInputs = () => {
@@ -34,10 +48,12 @@ const formValidate = () => {
 	const validate = document.getElementById('validate');
 	if (!titlePost.value && !textArea.value) {
 		validate.innerHTML = '*Input fields cannot be empty!';
+		validate.style.color = 'red';
 	} else if (!titlePost.value || !textArea.value) {
 		validate.innerHTML = '*One input field cannot be empty!';
+		validate.style.color = 'red';
 	} else {
-		validate.innerHTML = `<div class="">Created successfully!!</div>`;
+		validate.innerHTML = 'Created successfully!!';
 		validate.style.color = 'green';
 		clearInputs();
 	}
@@ -48,8 +64,8 @@ const submitFn = (event) => {
 	formValidate();
 
 	//storing to the localStorage
-	formData.push(newPost);
-	localStorage.setItem('formData', JSON.stringify(formData));
+	// formData.push(newPost);
+	// localStorage.setItem('formData', JSON.stringify(formData));
 };
 
 //Cancel button function
--- dist/scripts/mainpage.js
+++ dist/scripts/mainpage.js
@@ -1,1 +1,36 @@
 const mainPage = document.getElementById('main');
+
+const newNote = (formData) => {
+	html = `<div class="pb-1 hover:shadow-md hover:cursor-pointer">
+				<div
+					class="font-medium text-justify tracking-normal my-10 mx-10 xl:mx-72"
+				>
+					<h1 class="font-semibold text-2xl mb-2">
+						${formData.title}
+					</h1>
+					${formData.note}
+					<div class="mt-6 space-x-7 flex justify-end">
+						<button
+							class="bg-blue-600 text-white px-6 py-1.5 rounded-2xl hover:bg-blue-500"
+						>
+							Edit
+						</button>
+						<button
+							class="bg-red-600 text-white px-4 py-1.5 rounded-2xl hover:bg-red-500"
+						>
+							Delete
+						</button>
+					</div>
+				</div>
+			</div>`;
+
+	mainPage.insertAdjacentHTML('beforeend', newNote);
+};
+
+const createNewNote = () => {
+	const newNote = localStorage.getItem('formData');
+	JSON.parse(newNote).forEach((formData) => {
+		newNote(formData);
+	});
+};
+createNewNote();
